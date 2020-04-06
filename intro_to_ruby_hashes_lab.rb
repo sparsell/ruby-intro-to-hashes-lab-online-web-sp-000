19bb69c412f2c67f3fc2c95fbbded0738802d805
Sonja Parsell <hello@sparsell.com>
2020-04-06 13:11:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ def monopoly_with_third_tier
 			 pieces: 4,
 			 names: {},
 			 rent_in_dollars: {
-				 one_piece_owned: {},
+				 one_piece_owned: 25,
 				 two_pieces_owned: {},
 				 three_pieces_owned: {}
 			 }
